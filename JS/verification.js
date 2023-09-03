5705d3b958eeddd358280ffa38c6aa0edbbe8436
Lirys <iamyourmarkieboo@gmail.com>
2023-09-03 11:23:58 +0800

    Finished ageVerification page implementation

--- JS/verification.js
+++ JS/verification.js
@@ -1,15 +1,39 @@
 function sanitizeInput(){
-
-    const applied = new RegExp("[0-9]+");
-    const input = document.querySelector("#age");
-    if(document.querySelector(".error-text") === null) {
-        let err = document.createElement("span");
-        err.className = "error-text";
-        err.innerHTML = "test text";
-        err.style.flex = "1 0 100%";
-        err.style.textAlign = "center";
-        document.querySelector("#form-body").appendChild(err);
+    let applied = new RegExp("[0-9]+");
+    let invalidAge = new RegExp("-[0-9]+|^0");
+    let a = new RegExp("[a-zA-Z]+");
+    let input = document.querySelector("#age");
+    if(document.querySelector(".error-text") !== null){
+        document.querySelector(".error-text").remove()
+    }else if(a.test(input.value)){
+        createError("Only numbers are allowed!");
         return false;
     }
+    switch (input.value){
+        case "":
+            createError("Please fill out the field!");
+            return false;
+        default:
+            if(invalidAge.test(input.value)){
+                createError("Invalid age!");
+                return false;
+            }
+    }
     return applied.test(input.value);
+}
+
+function createError(message, textColor){
+    let err = document.createElement("span");
+    let inputErr = document.querySelector("#age");
+    err.className = "error-text";
+    err.innerHTML = message;
+    err.style.animation = "fadePop 2s";
+    err.style.color = (textColor === "" || textColor === undefined) ? "rgb(197, 22, 5)" : textColor;
+    inputErr.style.animation = "glowingRage 6.5s";
+    setInterval(()=> {
+        err.style.animation = "fadeOutSlide 1s";
+        inputErr.style.animation = "";
+        setInterval(() => err.remove(), 1000);
+    }, 5000);
+    return document.querySelector("#form-body").appendChild(err);
 }
--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <span id="title">Age verification</span>
         </div>
         <div class="body-container">
-            <form action="/Operations/verification.php" onsubmit="return sanitizeInput()" method="post" id="form-body">
+            <form action="<?php echo $_SERVER['PHP_SELF']?>" onsubmit="return sanitizeInput()" method="post" id="form-body">
                 <div class="form-row">
                     <input type="text" name="age" id="age">
                     <button type="submit">Verify Age</button>
@@ -28,3 +28,15 @@
     <script src="/JS/verification.js"></script>
 </body>
 </html>
+<?php
+if(isset($_POST["age"])){
+    $userAge = $_POST["age"];
+    if($userAge <= 17){
+        echo "<script>createError('You are under-aged!')</script>";
+    }elseif($userAge >=18 && $userAge <=149){
+        echo "<script>createError('You are allowed to vote!', 'limegreen')</script>";
+    }elseif($userAge >=150){
+        echo "<script>createError('Strange, however, you are allowed to vote!', 'yellow')</script>";
+    }
+}
+?>
